2941fd29292333236a48a292bc61d3613b9283b4
westhyun <98007431+westnowise@users.noreply.github.com>
2024-04-16 09:32:51 +0900

    Delete image_learning directory

--- image_learning/ft/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.urls import path
-# from .views import index, capture_image
-
-urlpatterns = [
-    path('image/', views.index, name='image'),
-    path('capture/', views.capture_image, name='capture_image'),
-]
