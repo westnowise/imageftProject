69320971098a10caaa5c257f73ad3c19b9078a71
westhyun <98007431+westnowise@users.noreply.github.com>
2024-04-16 09:33:22 +0900

    Delete image_learning directory

--- image_learning/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.urls import path
-# from .views import index, capture_image
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-]
