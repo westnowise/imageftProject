dcecf24750dd97e06e2a155948144478b5732c41
westhyun <s5681019@naver.com>
2024-04-22 17:20:04 +0900

    link

--- project1/urls.py
+++ project1/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('ft.urls')),
     path('game/', include('game.urls')),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
